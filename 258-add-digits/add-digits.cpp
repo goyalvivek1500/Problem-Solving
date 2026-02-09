40cc9a4a7a9978ca7675540c62db5be9d0f64f62
Vivek Goyal <vivekgoyal1500@gmail.com>
2026-02-09 15:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (76.96%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int rem,ans;
+        while(num>9)
+        {
+            ans=0;
+            while(num>0)
+            {
+                rem=num%10;
+                num/=10;
+                ans+=rem;
+            }
+            num=ans;
+        }
+        return num;
+    }
+};
