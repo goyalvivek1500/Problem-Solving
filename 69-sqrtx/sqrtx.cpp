68a8e61aea5bb28953923e4eecfaa091ba22f09b
Vivek Goyal <vivekgoyal1500@gmail.com>
2026-02-09 16:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (84.83%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        int start=1,end=x,ans=0;
+        while(start<=end)
+        {
+            int mid=(start)+(end-start)/2;
+            if(mid==x/mid)
+            {
+                return mid;
+            }
+            else if(mid>x/mid)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                ans=mid;
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+};
