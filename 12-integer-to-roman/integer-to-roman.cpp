83409e6839ba6504f61d6e0de3715d69d9cf3f6b
Vivek Goyal <vivekgoyal1500@gmail.com>
2026-02-15 13:56:54 +0530

    Time: 1 ms (73.70%) | Memory: 9.2 MB (78.50%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+   string convert(int nums)
+   {
+     switch(nums)
+     {
+        case 3000:return "MMM";
+        case 2000:return "MM";
+        case 1000:return "M";
+        case 900:return "CM";
+        case 800:return "DCCC";
+        case 700:return "DCC";
+        case 600:return "DC";
+        case 500:return "D";
+        case 400:return "CD";
+        case 300:return "CCC";
+        case 200:return "CC";
+        case 100:return "C";
+        case 90:return "XC";
+        case 80:return "LXXX";
+        case 70:return "LXX";
+        case 60:return "LX";
+        case 50:return "L";
+        case 40:return "XL";
+        case 30:return "XXX";
+        case 20:return "XX";
+        case 10:return "X";
+        case 9:return "IX";
+        case 8:return "VIII";
+        case 7:return "VII";
+        case 6:return "VI";
+        case 5:return "V";
+        case 4:return "IV";
+        case 3:return "III";
+        case 2:return "II";
+        case 1:return "I";
+        default: return "";
+     }
+   }
+
+    string intToRoman(int num) {
+        int thousands=(num/1000)*1000;
+        num=num%1000;
+        int hundreds=(num/100)*100;
+        num=num%100;
+        int tens=(num/10)*10;
+        num=num%10;
+        string ans="";
+        ans+=convert(thousands);
+        ans+=convert(hundreds);
+        ans+=convert(tens);
+        ans+=convert(num);
+        return ans;
+
+    }
+};
