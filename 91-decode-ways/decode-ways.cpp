2971d29cfa6c1b86e6f6a0e82f4e74ef10196d08
Vivek Goyal <vivekgoyal1500@gmail.com>
2026-03-09 22:26:34 +0530

    Time: 3 ms (18.33%) | Memory: 9.5 MB (22.31%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+        int n = s.size();
+        if(s[0] == '0') return 0;
+        
+        vector<int> dp(n + 1);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
+        {
+            int one = stoi(s.substr(i-1,1));
+            int two = stoi(s.substr(i-2,2));
+            
+            if(one >= 1)
+                dp[i] += dp[i-1];
+            
+            if(two >= 10 && two <= 26)
+                dp[i] += dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
